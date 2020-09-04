35227b626a9b28ec35a4ad971f60bdbca4298a68
irfanahmad7006 <56349613+irfanahmad7006@users.noreply.github.com>
2020-09-05 00:15:40 +0530

    Update BinarySearchTree.java

--- BinarySearchTree.java
+++ BinarySearchTree.java
@@ -1,1 +1,12 @@
-
+/* Node Structure
+  public class Node {
+    Node left; //Left_Reference
+    int data; //Data
+    Node right; //Right_Reference
+    Node(int value) { //Defining Constructor
+        this.data = value; //Storing the value in the current node
+        left = null; // Although it is not required, this for understanding purpose as in java both left & right variable will intialized with null by default.
+        right = null; // Although it is not required, this for understanding purpose as in java both left & right variable will intialized with null by default.
+    }
+}
+*/
